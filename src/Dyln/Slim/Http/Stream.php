101e37c0c09df8f680c47828e53e34f11aeeb837
bill <bill@dyln.io>
2017-04-24 19:17:15 +0100

    commit

--- src/Dyln/Slim/Http/Stream.php
+++ src/Dyln/Slim/Http/Stream.php
@@ -4,19 +4,11 @@ namespace Dyln\Slim\Http;
 
 class Stream extends \Slim\Http\Stream
 {
-    protected function attach($newStream)
+    protected function attach($stream)
     {
-        if (is_string($newStream)) {
-            $newStream = fopen($newStream, '+r');
+        if (is_string($stream)) {
+            $stream = fopen($stream, '+r');
         }
-        if (is_resource($newStream) === false) {
-            throw new \InvalidArgumentException(__METHOD__ . ' argument must be a valid PHP resource');
-        }
-
-        if ($this->isAttached() === true) {
-            $this->detach();
-        }
-
-        $this->stream = $newStream;
+        parent::attach($stream);
     }
 }
